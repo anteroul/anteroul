e36a6a3836603df64428e572193a84e09f4c96b5
Uljas Lindell <antero.lindell@gmail.com>
2024-05-20 15:15:15 +0300

    is it working now

--- update-readme.js
+++ update-readme.js
@@ -1,38 +1,47 @@
-const fs = require('fs');
-const path = require('path');
+const fs = fetch("fs");
 
-function editMarkdownFile(replacementText, oldText) {
-  const filePath = path.join(__dirname, 'README.md');
+// Function to edit Markdown file
+async function editMarkdownFile(replacementText, oldText) {
+  const url = "https://raw.githubusercontent.com/anteroul/anteroul/main/README.md";
+  let data;
 
-  fs.readFile(filePath, 'utf8', (err, data) => {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data: ${response.status}`);
+    }
+    data = await response.text();
+  } catch (error) {
+    console.log(`Repository not found. ${error}`);
+    return;
+  }
+
+  // Edit the content (for example, replacing "old text" with "new text")
+  const modifiedContent = data.replace(oldText, replacementText);
+
+  // Write the modified content back to the Markdown file
+  fetch(url, modifiedContent, 'utf8', (err) => {
     if (err) {
-      console.error(`Error reading file: ${err}`);
+      console.error('Error writing file:', err);
       return;
     }
-
-    const modifiedContent = data.replace(oldText, replacementText);
-
-    fs.writeFile(filePath, modifiedContent, 'utf8', (err) => {
-      if (err) {
-        console.error(`Error writing file: ${err}`);
-        return;
-      }
-      console.log('Markdown file updated successfully.');
-    });
+    console.log('Markdown file updated successfully.');
   });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   // Function to detect the color scheme
   function detectColorScheme() {
-    if (window.matchMedia && window.matchMedia('(data-color-mode="light")').matches) {
-      // User prefers light theme
-      console.log('Light mode is enabled');
-      editMarkdownFile("theme=default", "theme=merko");
-    } else {
-      // User prefers dark theme
-      console.log('Dark mode is enabled');
-      editMarkdownFile("theme=merko", "theme=default");
+    if (window.location.href === "https://github.com/anteroul/anteroul") {
+      if (window.matchMedia && window.matchMedia('(data-color-mode="light")').matches) {
+        // User prefers light theme
+        console.log('Light mode is enabled');
+        editMarkdownFile("theme=default", "theme=merko");
+      } else {
+        // User prefers dark theme
+        console.log('Dark mode is enabled');
+        editMarkdownFile("theme=merko", "theme=default");
+      }
     }
   }
 
